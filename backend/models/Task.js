bdadf68fd1d15f2e8b976ace797ebfaaee1c545c
El-JareauFlier <91876956+El-JareauFlier@users.noreply.github.com>
2024-06-06 13:27:02 +0200

    backend build

---
+++ backend/models/Task.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+//maak een database schema aan, zodat in de database een plek komt voor
+//de titel van de taak, beschrijving, of het compleet is en wanneer het is 
+//aangemaakt.
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+export default Task;
