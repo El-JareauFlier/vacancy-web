ffa1888ea48aa9e5ab0c49ed78cd8b7a22f3c3a4
El-JareauFlier <91876956+El-JareauFlier@users.noreply.github.com>
2024-06-06 14:29:03 +0200

    frontendStructure added

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,4 +1,5 @@
 import Task from '../models/Task.js';
+const mongoose = require('mongoose');
 
 export const getAllTasks = async (req, res) => {
   try {
@@ -21,4 +22,46 @@ export const createTask = async (req, res) => {
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
+};
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params; // Haal het taak-ID op uit de URL-parameter
+
+  try {
+    await Task.findByIdAndDelete(id); // Zoek de taak op basis van het ID en verwijder deze
+
+    res.status(200).json({ message: 'Task deleted successfully' }); // Retourneer een succesbericht als de taak succesvol is verwijderd
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' }); // Als er een fout optreedt, retourneer een 500-fout
+  }
+};
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params; // Haal het taak-ID op uit de URL-parameter
+
+  try {
+    let task = await Task.findById(id); // Zoek de taak op basis van het ID
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' }); // Als de taak niet wordt gevonden, retourneer een 404-fout
+    }
+
+    task = await Task.findByIdAndUpdate( // Update de taak met de gegevens uit het verzoeklichaam
+      id, // Zoekopdracht op basis van het ID
+      {
+        ...req.body, // Gebruik de gegevens uit het verzoeklichaam om de taak te updaten
+      },
+      { new: true } // Geeft de bijgewerkte taak terug in plaats van de oude
+    );
+
+    res.status(200).json(task); // Retourneer de bijgewerkte taak als JSON met statuscode 200
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' }); // Als er een fout optreedt, retourneer een 500-fout
+  }
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  deleteTask,
+  updateTask
 };
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllTasks, createTask } from '../controllers/taskController.js'
+import { getAllTasks, createTask, deleteTask, updateTask } from '../controllers/taskController.js'
 
 const router = express.Router();
 
@@ -8,5 +8,12 @@ router.get('/', getAllTasks);
 
 // Endpoint om een nieuwe taak toe te voegen
 router.post('/', createTask);
+
+//Eindpoint om een taak te verwijderen
+router.delete('/:id', deleteTask);
+
+//Eindpoint om een taak te updaten
+router.put('/:id', updateTask);
+
 
 export default router;
